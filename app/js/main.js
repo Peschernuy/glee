8cc288a743fa941391079ca243b2f94c9a7699af
peschernuy <peschernuy@gmail.com>
2020-12-04 00:47:14 +0200

    add filter part in product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
 $(function(){
 
+  $(".star").rateYo({
+
+    starWidth: "12px", 
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+           $('.filter-price__from').text(data.from);
+           $('.filter-price__to').text(data.to);
+
+    },
+    
+  })
+
   $('.top-slider__inner').slick({
     dots:true,
     arrows: false,
